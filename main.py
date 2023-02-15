06089a2ff7dd7a3968b6d1322544779a532231c4
Shivansh Goel <shivanshggamer@gmail.com>
2023-02-15 10:25:11 -0800

    DB Layer Working

--- main.py
+++ main.py
@@ -1,26 +1,32 @@
+import threading
+
 # import "packages" from flask
 from flask import render_template  # import render_template from "public" flask libraries
-# import "packages" from "this" project
-from __init__ import app  # Definitions initialization
-from api import app_api # Blueprint import api definition
-from bp_projects.projects import app_projects # Blueprint directory import projects definition
 
-app.register_blueprint(app_api) # register api routes
-app.register_blueprint(app_projects) # register api routes
+from __init__ import app
+from model.snake import initGamers
+
+# setup APIs
+from api.snake import snake_api # Blueprint import api definition
+
+# register URIs
+app.register_blueprint(snake_api) # register app pages
+
 
 @app.errorhandler(404)  # catch for URL not found
 def page_not_found(e):
-    # note that we set the 404 status explicitly
     return render_template('404.html'), 404
 
-@app.route('/')  # connects default URL to index() function
+@app.route('/')  
 def index():
     return render_template("index.html")
 
-@app.route('/stub/')  # connects /stub/ URL to stub() function
-def stub():
-    return render_template("stub.html")
+@app.before_first_request
+def activate_job():
+    initGamers()
+
 
 # this runs the application on the development server
 if __name__ == "__main__":
-    app.run(debug=True)
+    #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///snake.db'
+    app.run(debug=True, host="0.0.0.0", port="8086")
